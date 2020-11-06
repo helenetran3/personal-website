2f689e835c4432257e6bf63c7652045c974f23ea
helenetran3 <helene.tran@ensiie.fr>
2020-11-06 17:43:06 +0100

    Create a separate page for Contact page. Change photos into Card object.

--- presentation/urls.py
+++ presentation/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'presentation'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
 ]
--- presentation/views.py
+++ presentation/views.py
@@ -4,3 +4,7 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'presentation/index.html'
+
+
+class ContactView(generic.TemplateView):
+    template_name = 'presentation/contact.html'
