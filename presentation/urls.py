d6b83a3fd73e78c5dfe945051b2555373fc55aae
helenetran3 <helene.tran@ensiie.fr>
2020-11-11 22:02:28 +0100

    Simple form for contact page works with no problem. Add new section About me with black background.

--- presentation/urls.py
+++ presentation/urls.py
@@ -6,4 +6,6 @@ app_name = 'presentation'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('contact/', views.ContactView.as_view(), name='contact'),
+    path('contact_submitted/', views.SubmittedView.as_view(), name='submitted'),
+    path('contact/submit/', views.get_form, name='submit_form'),
 ]
--- presentation/views.py
+++ presentation/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse
+from .forms import ContactForm
 
 
 class IndexView(generic.TemplateView):
@@ -8,3 +11,26 @@ class IndexView(generic.TemplateView):
 
 class ContactView(generic.TemplateView):
     template_name = 'presentation/contact.html'
+
+
+class SubmittedView(generic.TemplateView):
+    template_name = 'presentation/submitted.html'
+
+
+def get_form(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ContactForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('presentation:submitted'))
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ContactForm(None)
+
+    return render(request, 'presentation/contact.html', {'form': form})
